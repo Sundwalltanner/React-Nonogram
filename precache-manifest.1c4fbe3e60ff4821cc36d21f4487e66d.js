abb5541e2f47e39c61a21627f7ff4335dbc7c89c
Tanner Sundwall <sundwalltanner@gmail.com>
2020-03-17 10:51:18 -0700

    Updates

---
+++ precache-manifest.1c4fbe3e60ff4821cc36d21f4487e66d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bbdc0e8efe2e9d79f939610f8ffbdb5",
+    "url": "/react-nonogram/index.html"
+  },
+  {
+    "revision": "9f8b426eece6af05205f",
+    "url": "/react-nonogram/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "eedfa920dcf7bfc7dc33",
+    "url": "/react-nonogram/static/js/2.6afe9531.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-nonogram/static/js/2.6afe9531.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f8b426eece6af05205f",
+    "url": "/react-nonogram/static/js/main.d397d3fa.chunk.js"
+  },
+  {
+    "revision": "42738d53be9c9799330a",
+    "url": "/react-nonogram/static/js/runtime-main.bc2643d1.js"
+  }
+]);
